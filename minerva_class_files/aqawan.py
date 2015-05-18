5a083f397c0a9af2b60bfa82a46d0bd2f2d1a013
jdeast <jdeast@gmail.com>
2015-05-17 18:47:20 -0700

    fix bug in status logging

--- minerva_class_files/aqawan.py
+++ minerva_class_files/aqawan.py
@@ -213,7 +213,7 @@ class aqawan:
     # get aqawan status
     def status(self):
         response = self.send('STATUS').split(',')
-        self.logger.info("Status: " + response)
+        self.logger.info("Status: " + str(response))
         status = {}
         for entry in response:
             if '=' in entry:
