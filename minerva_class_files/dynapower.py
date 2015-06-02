000481e6f5c6e2e5775392bd9feaea1df60052a6
jdeast <jdeast@gmail.com>
2015-06-03 04:35:59 +1200

    dynapower code written (untested)

--- minerva_class_files/dynapower.py
+++ minerva_class_files/dynapower.py
@@ -2,8 +2,9 @@
 create class object by powerswitch(num), where num specify which powerswitch
 test program creates powerswitch(1) object and send keyboard commands'''
 
-import time, telnetlib, socket, threading, logging, ipdb, datetime, json
+import time, urllib2, ipdb, datetime, logging, requests
 from configobj import ConfigObj
+from requests.auth import HTTPBasicAuth
 
 #To Do: change log to appropriate format, log open/close failure by reading status, add more functionality as needed 
 class dynapower:
@@ -47,162 +48,50 @@ class dynapower:
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(fileHandler)
         self.logger.addHandler(console)
-            
-    #heartbeat thread function
-    def on(self,outlet):
 
-    	self.ip + 
-        return self.send('HEARTBEAT')
 
-    def crack(self):
-        self.send('OPEN_SHUTTER_1')
-        self.send('STOP')
+    def send(self,url):
 
-    #send message to aqawan
-    def send(self,message):
-            
-        messages = ['HEARTBEAT','STOP','OPEN_SHUTTERS','CLOSE_SHUTTERS',
-                    'CLOSE_SEQUENTIAL','OPEN_SHUTTER_1','CLOSE_SHUTTER_1',
-                    'OPEN_SHUTTER_2','CLOSE_SHUTTER_2','LIGHTS_ON','LIGHTS_OFF',
-                    'ENC_FANS_HI','ENC_FANS_MED','ENC_FANS_LOW','ENC_FANS_OFF',
-                    'PANEL_LED_GREEN','PANEL_LED_YELLOW','PANEL_LED_RED',
-                    'PANEL_LED_OFF','DOOR_LED_GREEN','DOOR_LED_YELLOW',
-                    'DOOR_LED_RED','DOOR_LED_OFF','SON_ALERT_ON',
-                    'SON_ALERT_OFF','LED_STEADY','LED_BLINK',
-                    'MCB_RESET_POLE_FANS','MCB_RESET_TAIL_FANS',
-                    'MCB_RESET_OTA_BLOWER','MCB_RESET_PANEL_FANS',
-                    'MCB_TRIP_POLE_FANS','MCB_TRIP_TAIL_FANS',
-                    'MCB_TRIP_PANEL_FANS','STATUS','GET_ERRORS','GET_FAULTS',
-                    'CLEAR_ERRORS','CLEAR_FAULTS','RESET_PAC']
-        # not an allowed message
-        if not message in messages:
-            self.logger.error('Message not recognized: ' + message)
-            return -1
-        
-        port = 22004
-        self.lock.acquire()
-        try:
-            tn = telnetlib.Telnet(self.IP,port,1)
-        except socket.timeout:
-            self.logger.error('Timeout attempting to connect to the aqawan')
-            self.lock.release()
-            return -1
-
-        tn.write("vt100\r\n")
-
-        # why is this necessary!? this is quite unsettling
-        response = ''
-        while response == '':
-            tn.write(message + "\r\n")
-            response = tn.read_until(b"/r/n/r/n#>",0.5)
-            
-        tn.close()
-        self.lock.release()
+        f = open('dynapower.txt','r')
+        username = f.readline().strip()
+        password = f.readline().strip()
+        f.close()
+        self.logger.info('Sending command: ' + url)
+        response = requests.get(url,auth=(username, password))
+        self.logger.info('Response code = ' + str(response.status_code))
         return response
 
-    #open both shutters
-    def open_both(self):
+    def getoutletstr(self,outlet)
+        outletstr = ''
+        for i in range(8):
+            if i == outlet: outletstr = outletstr + '1'
+            else: outletstr = outletstr + '0'
+        outletstr = outletstr + '00000000' + '00000000'
+        return outletstr
+    
+    def on(self,outlet):
+        outletstr = self.getoutletstr(outlet)
+        url = 'http://' + self.IP + '/ons.cgi?led=' + outletstr
+        return self.send(url)
 
-        self.logger.info('Shutting off lights')
-        response = self.send('LIGHTS_OFF')
-        if response == -1:
-            self.logger.error('Could not turn off lights')
+    def off(self,outlet):
+        outletstr = self.getoutletstr(outlet)
+        url = 'http://' + self.IP + '/offs.cgi?led=' + outletstr
+        return self.send(url)
+    
+    def cycle(self,outlet,cycletime=None):
 
-        self.logger.info('Opening shutter 1')
-        response = self.open_shutter(1)
-        if response == -1: return -1
-        self.logger.info('Shutter 1 open')
-
-        self.logger.info('Opening shutter 2')
-        response = self.open_shutter(2)
-        if response == -1: return -1
-        self.logger.info('Shutter 2 open')
-
-        self.isOpen = True
-            
-    def open_shutter(self,shutter):
-        # make sure this is an allowed shutter
-        if shutter not in [1,2]:
-            self.logger.error('Invalid shutter specified (' + str(shutter) + ')')
-            return -1
-
-        status = self.status()
-        timeout = 180.0
-        elapsedTime = 0.0
-
-        # if it's already open, return
-        if status['Shutter' + str(shutter)] == 'OPEN':
-            self.logger.info('Shutter ' + str(shutter) + ' already open')
-            return
-
-        # open the shutter
-        start = datetime.datetime.utcnow()
-        response = self.send('OPEN_SHUTTER_' + str(shutter))                
-        self.logger.info(response)
-        if not 'Success=TRUE' in response:
-            # did the command fail?
-            self.logger.warning('Failed to open shutter ' + str(shutter) + ': ' + response)
-            ipdb.set_trace()
-            # need to reset the PAC? ("Enclosure not in AUTO"?)
-        
-        # Wait for it to open
-        self.logger.info('Waiting for shutter ' + str(shutter) + ' to open')
-        status = self.status()
-        while status['Shutter' + str(shutter)] == 'OPENING' and elapsedTime < timeout:
-            status = self.status()
-            elapsedTime = (datetime.datetime.utcnow()-start).total_seconds()
-
-        # Did it fail to open?
-        if status['Shutter' + str(shutter)] <> 'OPEN':
-            self.logger.error('Error opening Shutter ' + str(shutter) )
-            return -1
-
-        self.logger.info('Shutter ' + str(shutter) + ' open')
-            
-    #close both shutter
-    def close_both(self):
-        timeout = 500
-        elapsedTime = 0
-        self.isOpen = False
-        status = self.status()
-        if status['Shutter1'] == "CLOSED" and status['Shutter2'] == "CLOSED":
-            self.logger.debug('Both shutters already closed')
+        if cycletime == None:
+            outletstr = self.getoutletstr(outlet)
+            url = 'http://' + self.IP + '/offon.cgi?led=' + outletstr
+            return self.send(url)
         else:
-            response = self.send('CLOSE_SEQUENTIAL')
-            if not 'Success=TRUE' in response:
-                self.logger.error('Aqawan failed to close!')
-                self.isOpen = True
-                # need to send alerts, attempt other stuff
-            else:
-                self.logger.info(response)    
-                start = datetime.datetime.utcnow()
-                while (status['Shutter1'] <> "CLOSED" or status['Shutter2'] <> "CLOSED") and elapsedTime < timeout:
-                    elapsedTime = (datetime.datetime.utcnow() - start).total_seconds()
-                    status = self.status()
-                if status['Shutter1'] <> "CLOSED" or status['Shutter2'] <> "CLOSED":
-                    self.logger.error('Aqawan failed to close after ' + str(elapsedTime) + 'seconds!')
-                    self.isOpen = True
-                    # need to send alerts, attempt other stuff
-                else:
-                    self.logger.info('Closed both shutters')
-                    self.lastClose = datetime.datetime.utcnow()
-            
-    # get aqawan status
-    def status(self):
-        response = self.send('STATUS').split(',')
-        status = {}
-        for entry in response:
-            if '=' in entry:
-                status[(entry.split('='))[0].strip()] = (entry.split('='))[1].strip()
-
-        with open(self.currentStatusFile,'w') as outfile:
-            json.dump(status,outfile)
-
-        return status         
+            self.off(outlet)
+            time.sleep(cycletime)
+            return self.on(outlet)  
 
 if __name__ == '__main__':
 
-    aqawan_1 = aqawan(1)
-    while True:
-        command = raw_input('enter Aqawan command: ')
-        print aqawan_1.send(command)
+    d2 = dynapower('D2','n20150521',configfile='minerva_class_files/dynapower.ini')
+    print d2.cycle(8)
+    
