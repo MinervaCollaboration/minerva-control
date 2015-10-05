d184cb6d91d389aaa8c2c97975b0cfe518d21dd6
minerva <minerva@Main.(none)>
2015-10-05 15:16:47 -0700

    change reboot message; now knows about daily reboot

--- minerva_library/startup.py
+++ minerva_library/startup.py
@@ -1,10 +1,11 @@
 import subprocess, os, socket, time
 import mail
+import datetime
 
 hostname = socket.gethostname()
 
 body = "Dear benevolent humans,\n\n" + \
-       "I require your assistance to recover from a recent power outage. Please:\n\n" + \
+       "If someone did not intentionally reboot the me, I require your assistance to recover from a recent power outage. Please:\n\n" + \
        "1) Make sure the telescope is connected, the drives are enabled, and the tracking is off.\n" + \
        "2) Home the telescope\n" + \
        "   * If this fails, you may need to power cycle the 'T# panel' and start over:\n" + \
@@ -24,6 +25,19 @@ body = "Dear benevolent humans,\n\n" + \
        "Love,\n" + \
        "MINERVA"
 
-mail.send(hostname + " rebooted during observations",body,level="serious")
+body2 = "Dear benevolent humans,\n\n" + \
+       "I have successfully completed my daily reboot. No action is required.\n\n" + \
+       "Love,\n" + \
+       "MINERVA"
+
+now = datetime.datetime.now()
+
+# if it's between 3:30 and 3:45, it's part of the normal daily reboot
+timediff = (now - datetime.datetime(now.year, now.month,now.day,15,30)).total_seconds()
+if timediff > 0 and timediff < 900:
+    mail.send(hostname + " finished daily reboot",body2,level="normal")
+else:
+    # otherwise, there was probably a power outage
+    mail.send(hostname + " rebooted unexpectedly and requires assistance",body,level="serious")
 
 time.sleep(60)
