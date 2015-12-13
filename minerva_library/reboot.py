2e92dc1ad21d222b4890b1bae7f9f86eae0944e2
jdeast <jdeast@gmail.com>
2015-12-13 16:45:09 -0700

    fix reboot.py

--- minerva_library/reboot.py
+++ minerva_library/reboot.py
@@ -1,30 +1,33 @@
 import imager, cdk700
-import os, socket, time, subprocess
+import os, socket, time, subprocess, psutil
 import ipdb
 
 config_file = 'imager_t' + socket.gethostname()[1] + '.ini'
 base_directory = 'C:\minerva-control'
 
-try: camera = imager.imager(config_file,base_directory)
-except: pass
+for p in psutil.process_iter():
+    try:
+        if p.name() == 'MaxIm_DL.exe':
+            try:
+                camera = imager.imager(config_file,base_directory)
+                # disconnect from the camera
+                try: camera.disconnect_camera()
+                except: pass
+                try: subprocess.call(['Taskkill','/IM','MaxIm_DL.exe','/F'])
+                except: pass
+            except: pass
 
-# disconnect from the camera
-try: camera.disconnect_camera()
-except: pass
+        elif p.name() == 'PWI.exe':
+            telescope = cdk700.CDK700('telescope_' + socket.gethostname()[1] + '.ini', base_directory)
 
-try: subprocess.call(['Taskkill','/IM','MaxIm_DL.exe','/F'])
-except: pass
+            # disconnect from mount
+            try: telescope.shutdown()
+            except: pass
 
-# disconnect from rotator
-telescope = cdk700.CDK700('telescope_' + socket.gethostname()[1] + '.ini', base_directory)
-
-# disconnect from mount
-try: telescope.shutdown()
-except: pass
-
-# kill PWI
-try: telescope.killPWI()
-except: pass
+            # kill PWI
+            try: subprocess.call(['Taskkill','/IM','PWI.exe','/F'])
+            except: pass
+    except: pass
 
 # reboot computer
 os.system('shutdown -r -t 60')
