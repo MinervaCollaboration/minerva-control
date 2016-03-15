b2ff49f0ee278aa00e5d3b7b4c46ebfdd15f069a
minerva <minerva@Main.(none)>
2016-03-15 13:39:19 -0700

    fix nan check

--- minerva_library/imager_server.py
+++ minerva_library/imager_server.py
@@ -266,10 +266,12 @@ class server:
 
 			for key,value in hdr.iteritems():
 				if isinstance(value, (str, unicode)):
-					if math.isnan(value): value = 'NaN'
+					if isinstance(value,float):
+						if math.isnan(value): value = 'NaN'
 					f[0].header[key] = value
 				else:
-					if math.isnan(value[0]): value[0] = 'NaN'
+					if isinstance(value[0],float):
+						if math.isnan(value[0]): value[0] = 'NaN'
 					f[0].header[key] = (value[0],value[1])
 			f.flush()
 			f.close()
@@ -503,10 +505,3 @@ if __name__ == '__main__':
 	
     test_server = server(config_file,base_directory)
     test_server.run_server()
-	
-	
-	
-	
-	
-	
-	
