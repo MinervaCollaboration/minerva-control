34c93c506c5ffc2228653e1cbc48f4cd9c0112d5
jdeast <jdeast@gmail.com>
2016-03-31 03:47:04 -0700

    it does appear to be fixed now...

--- minerva_library/imager_server.py
+++ minerva_library/imager_server.py
@@ -269,9 +269,10 @@ class server:
 						if math.isnan(value): value = 'NaN'
 					f[0].header[key] = value
 				else:
-					if isinstance(value[0],float):
-						if math.isnan(value[0]): value[0] = 'NaN'
-					f[0].header[key] = (value[0],value[1])
+       					if isinstance(value[0],float):
+               					if math.isnan(value[0]): value[0] = 'NaN'
+                       			f[0].header[key] = (value[0],value[1])
+
 			f.flush()
 			f.close()
 		except:
